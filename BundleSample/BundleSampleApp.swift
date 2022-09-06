1bde608d7a3df9edb2f540b9342c4aa3aacde146
Alexander von Below <alex@vonbelow.com>
2022-09-06 10:09:31 +0200

    Initial Commit

---
+++ BundleSample/BundleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BundleSampleApp.swift
+//  BundleSample
+//
+//  Created by Alexander von Below on 06.09.22.
+//
+
+import SwiftUI
+
+@main
+struct BundleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
